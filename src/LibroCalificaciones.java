b2f4b303fbfe9651e0d6ee93f8f9e169649ddae5
springalwayspro <springalwayspro@gmail.com>
2021-05-27 01:41:55 -0400

    Capitulo 03 Deite y Deitel

---
+++ src/LibroCalificaciones.java
@@ -0,0 +1,18 @@
+// Fig. 3.1 LibroCalificaciones
+// Declaraciòn de un clase con un nmètodo...
+
+public class LibroCalificaciones {
+
+    //Mostrar un mensaje de bienvenida Libro de calificaciones
+    public void mostrarMensaje(){
+
+        System.out.println("Bienvenido al libro de calificaciones !!!");
+    } // Fin del mètodo mostraMensaje()
+} // Fin de la clase LibroCalificaciones
+
+
+/* Capitulo 03 de Deitel & Deitel.
+*
+*
+*
+* */
---
+++ src/PruebaLibroCalificaciones.java
@@ -0,0 +1,16 @@
+// Fig 3.2 PruebaLibroCalificaciones.java
+// Crea un objeto LibroCalificaciones y llama a su mètodo mostarMensaje
+
+public class PruebaLibroCalificaciones {
+
+    // El mètodo main empieza la ejecuciòn del programa
+    public static void main(String[] args) {
+
+        // Crea un objeto LibroCalificaciones y lo asigna a miLibroCalificaciones
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+
+        System.out.println();
+        // llama el mètodo mostraMensaje de miLibroCalificaciones
+        miLibroCalificaciones.mostrarMensaje();
+    } // fin de main
+} // fin de la clase PruebaLibroCalificaciones
